dceb0419b010e66910f3be228de00493b0fa6828
sirclancy <clancymudaki@gmail.com>
2022-06-03 10:58:57 +0300

    Store and Cart Functionality

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from tracemalloc import is_tracing
 from django.urls import reverse
 from django.db import models
 from category.models import Category
@@ -21,3 +22,34 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+# creating variations manager since _set.all fetches both color and size 
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category = 'color', is_active = True)
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category = 'size', is_active = True)
+
+
+
+# creating a tuple for the variations
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=200, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default = True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+
+    def __str__(self):
+        return self.variation_value
